6f7650661366f597f2592e78c63c64456b4cb2d4
Alexander Kalinin <alex@kalinin.uk>
2021-04-05 14:22:03 +0100

    metadata

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,6 +9,7 @@ export default function Layout({ children, pageTitle, description }: any): JSX.E
         <meta charSet="utf-8" />
         <meta name="Description" content={description}></meta>
         <title>{pageTitle}</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <style jsx global>{`
         @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap|Roboto:wght@400;700&display=swap');
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import { Post } from '../components/Post';
 import { posts } from '../components/getAllPosts';
 import { NextPage } from 'next';
+import Head from 'next/head';
 
 const indexPage: NextPage = () => (
   <>
+    <Head>
+      <title>{"Alex Kalinin's Blog"}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <meta
+        name="Description"
+        content="My blog, talking about several of my projects, and some guides."
+      ></meta>
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </Head>
     {posts.map((post) => (
       <Post key={post.link} post={post} />
     ))}
