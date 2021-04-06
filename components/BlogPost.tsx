0fc31cac935564840c73411a169fc8be2674902f
Alexander Kalinin <alex@kalinin.uk>
2021-04-06 13:25:28 +0100

    favicon

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -7,6 +7,12 @@ export default function BlogPost({ children, meta }: PostModule): JSX.Element {
     <>
       <Head>
         <meta name="Description" content={meta.description}></meta>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c"></meta>
+        <meta name="theme-color" content="#ffffff"></meta>
         <title>{meta.title}</title>
       </Head>
       <h1 className="great-title">{meta.title}</h1>
