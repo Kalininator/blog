34a4cd576ec7c8051676a95968b158b8d2dd757b
Alexander Kalinin <alex@kalinin.uk>
2021-04-06 22:49:07 +0100

    Layout styled-compnents

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,46 @@
 import Head from 'next/head';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: #282828;
+    color: #fbf1c7;
+    font-size: 1rem;
+  }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: 700;
+    font-family: 'Nunito', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+  a {
+    color: #fbf1c7;
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+    object-fit: contain;
+  }
+  pre {
+    white-space: pre-wrap;
+  }
+`;
+
+const Content = styled.div`
+  max-width: 600px;
+  margin: 2rem auto;
+  padding: 0 1rem;
+`;
 
 export default function Layout({
   children,
@@ -20,46 +61,11 @@ export default function Layout({
       </Head>
       <style jsx global>{`
         @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap|Roboto:wght@400;700&display=swap');
-        html,
-        body {
-          margin: 0;
-          padding: 0;
-          font-family: 'Roboto', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-            Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-          background: #282828;
-          color: #fbf1c7;
-          font-size: 1rem;
-        }
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-          font-weight: 700;
-          font-family: 'Nunito', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-            Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        }
-        a {
-          color: #fbf1c7;
-        }
-        img {
-          max-width: 100%;
-          height: auto;
-          object-fit: contain;
-        }
-        pre {
-          white-space: pre-wrap;
-        }
-        .content {
-          max-width: 600px;
-          margin: 2rem auto;
-          padding: 0 1rem;
-        }
       `}</style>
+      <GlobalStyle />
       <main>
         <Header />
-        <div className="content">{children}</div>
+        <Content>{children}</Content>
       </main>
     </>
   );
